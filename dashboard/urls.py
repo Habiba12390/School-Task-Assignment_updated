7b63bb841e3e270cc0dc15fdce4cc3befee492ac
Abdelkareem Ahmed <150220197+abdelkareeemahmed@users.noreply.github.com>
2026-05-07 11:07:54 +0300

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('admin-dashboard/', views.admin_dashboard),
     path('teacher-dashboard/', views.teacher_dashboard),
     path('teacher-complete/<str:task_id>/', views.teacher_complete_task),
+    path('api/admin-tasks/', views.get_admin_tasks),
+    path('api/delete-task/<str:task_id>/', views.admin_delete_task),
 ]
