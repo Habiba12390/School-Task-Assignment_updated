b1ba7ebb4cefe5bef9b1c2ed8042591229f50c72
Ahmed-Soliman7 <ahayfa06@gmail.com>
2026-05-08 15:47:00 +0300

    Update urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path  # type: ignore
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin-dashboard/', views.admin_dashboard),
-    path('teacher-dashboard/', views.teacher_dashboard),
-    path('teacher-complete/<str:task_id>/', views.teacher_complete_task),
-    path('api/admin-tasks/', views.get_admin_tasks),
-    path('api/delete-task/<str:task_id>/', views.admin_delete_task),
+    path('', views.task_list, name='task_list'),
+    path('add-task/', views.add_task_page, name='add_task'),
+    path('edit-task/', views.edit_task_page, name='edit_task'),
+    path('completed-tasks/', views.completed_tasks_page, name='completed_tasks'),
+    path('task-details/', views.task_details_page, name='task_details'),
+    path('<str:task_id>/', views.task_detail, name='task_detail'),
 ]
